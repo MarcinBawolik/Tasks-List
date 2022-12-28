98b8f9a47dc11d7bd6c1c4e31272f12374641ab6
Marcin Bawolik <marcinbawolik2000@gmail.com>
2022-12-28 14:17:49 +0100

    toggle task done

--- script.js
+++ script.js
@@ -9,21 +9,6 @@
             done: true,
         },
     ];
-
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
-                ${task.content}
-                </li>
-                `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -31,7 +16,52 @@
 
         render();
     }
-    const onFormSubmit =  (event) => {
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li
+                 ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+                 >
+                 <button class="js-done">zrobione?</button>
+                 <button class="js-remove">usuń</button>
+                ${task.content}
+                </li>
+                `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(taskIndex);
+            });
+
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+
+        });
+    };
+
+    const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
